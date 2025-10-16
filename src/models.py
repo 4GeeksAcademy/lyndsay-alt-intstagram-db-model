b99c79d7f28bbb6921b83e7d3ef16ab7b4130db4
Lyndsay R <richardslyndsay@gmail.com>
2025-10-16 18:44:44 +0000

    last

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
 
 db = SQLAlchemy()
 
@@ -13,14 +14,23 @@ class User(db.Model):
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
 
-class Task(db.Model):
-    done: Mapped[bool] = mapped_column(Boolean(), default=False)
-    creater_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
-    asignee_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
-
-
-class Profile(db.Model):
+class Post(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    dob: Mapped[datetime] = mapped_column(DateTime(timezone=timezone.utc))
-    address: Mapped[str] = mapped_column(String(512))
-    phone_number: Mapped[str] = mapped.colum()
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    content: Mapped[str] = mapped_column(String(500), nullable=False)
+
+
+class Comment(db.Model):
+    user_from_id: Mapped[int] = mapped_column(primary_key=True)
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+    content: Mapped[str] = mapped_column(String(500), nullable=False)
+
+
+class Follower(db.Model):
+    user_from_id: Mapped[int] = mapped_column(primary_key=True)
+    user_to_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+
+
+class Saved(db.Model):
+    user_from_id: Mapped[int] = mapped_column(primary_key=True)
+    liked: Mapped[str] = mapped_column(ForeignKey("post.id"))
